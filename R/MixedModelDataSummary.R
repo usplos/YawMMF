86e3c783d82bde08f167974a85d2be0979ffd1ba
Yaw <zgy070620@163.com>
2020-01-04 14:21:42 +0800

    Add files via upload

--- R/MixedModelDataSummary.R
+++ R/MixedModelDataSummary.R
@@ -3,9 +3,12 @@ MixedModelDataSummary = function(Data, DV = 'DV',Cond = 'CondA', Group = 'subj')
 
   DF = eval(parse(text = paste0('Data %>% group_by(',
                                 Cond,',',Group,')')))
-  DF = eval(parse(text = paste0('summarise(DF, Mean2 = mean(',DV,'))')))
+  DF = eval(parse(text = paste0('summarise(DF, Mean2 = mean(',DV,'),',
+                                'trialnum = length(',DV,'))')))
   DF = eval(parse(text = paste0('DF %>% group_by(',Cond,')')))
-  DF = eval(parse(text = paste0('summarise(DF, Mean = mean(Mean2), SE = sd(Mean2)/sqrt(length(Mean2)))')))
+  DF = eval(parse(text = paste0('summarise(DF, TrialNum = sum(trialnum), Mean = mean(Mean2), SD = sd(Mean2), SE = sd(Mean2)/sqrt(length(Mean2)))')))
+  names(DF)[which(names(DF) == 'SD')] = paste(DV, '_SD',sep = '')
+  names(DF)[which(names(DF) == 'TrialNum')] = paste(DV, '_TrialNum',sep = '')
   names(DF)[which(names(DF) == 'Mean')] = paste(DV, '_Mean',sep = '')
   names(DF)[which(names(DF) == 'SE')] = paste(DV, '_SE',sep = '')
   return(DF)
