ff92d4b2fdaff6f09baa4f41229444b8c991a3d0
Yaw <zgy070620@163.com>
2020-01-17 08:55:13 +0800

    Add files via upload

--- R/MixedModelWrite.R
+++ R/MixedModelWrite.R
@@ -8,5 +8,18 @@ MixedModelWrite = function(Model = NULL, Data = NULL, Prefix = 'DV',...){
   if(!is.null(Model)){
     Modelinfo = summary(Model)$coef %>% round(x = ., digits = 3) %>% as.data.frame()
     write.csv(Modelinfo, paste0(Prefix, '_Modelinfo.csv'))
+
+    qqp = ggplot(as_tibble(residuals(Model)), aes(sample = value))+
+      stat_qq(alpha = 0.2) + stat_qq_line()+
+      labs(title = 'QQPlot of the residual')+
+      theme(plot.title = element_text(hjust = 0.5))
+    denp = qplot(x = residuals(Model), geom = 'density',xlab = 'residuals', ylab = 'prop. density')+
+      labs(title = 'Propability density of residuals')+
+      theme(plot.title = element_text(hjust = 0.5))
+    if(!require(patchwork)) {devtools::install_github("thomasp85/patchwork")}
+    library(patchwork)
+    ps = qqp|denp
+    ggsave(filename = paste0(Prefix,'_Residual information plot.tiff'),plot = ps,device = 'tiff',
+           units = 'cm',width = 20,height = 10)
   }
 }
