9ebc598dc12455494bb30c065e44c85c0a324819
Yaw <zgy070620@163.com>
2020-04-15 13:03:14 +0800

    Add files via upload

---
+++ R/NORM_CLUST.R
@@ -0,0 +1,31 @@
+NORM_CLUST = function(N, pvalue = 0.05){
+  library(mclust);library(tidyverse)
+  p = 0
+  start = 1
+  DFs = tibble()
+  DF = tibble(N)
+  DF['label'] = 1:length(N)
+  DF['C'] = 0
+  NROW = nrow(DF)
+  while (NROW >= 10) {
+    G = 1
+    p = 0
+    while (p < pvalue) {
+      Model = Mclust(DF$N, G = G)
+      p = shapiro.test(DF$N[Model$classification == 1])$p.value
+      G = G+1
+    }
+
+    DF$C[Model$classification == 1] = start
+    DFs = rbind(DFs, DF %>% filter(C != 0))
+    DF = DF %>% filter(C == 0)
+    NROW = nrow(DF)
+    start = start+1
+  }
+  if(nrow(DF) > 0){
+    DF$C = start
+    DFs = rbind(DFs, DF)
+  }
+  return(DFs %>% arrange(label) %>% .$C)
+}
+
