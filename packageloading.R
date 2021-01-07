374474717632838c23f3318ece5913a2a61a0d91
Yaw <zgy070620@163.com>
2021-01-07 19:05:50 +0800

    Create packageloading.R

---
+++ packageloading.R
@@ -0,0 +1,5 @@
+.onAttach=function(libname, pkgname) {
+
+  cat('######################\nPlease cite:\n')
+  cat('Zhang, G., Li, X., & Lin, N. (2020). YawMMF: Effective Mixed Model Functions. Available at: https://github.com/usplos/YawMMF.\n######################\n')
+}
