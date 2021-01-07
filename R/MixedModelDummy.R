dae23f05c007b7a7b01e94ca0a0a43e611f5b825
Yaw <zgy070620@163.com>
2021-01-07 16:49:30 +0800

    Update MixedModelDummy.R

--- R/MixedModelDummy.R
+++ R/MixedModelDummy.R
@@ -1,4 +1,8 @@
 MixedModelDummy = function(Data, FixEffects){
+  
+cat('please cite: \n')
+cat('Zhang, G., Li, X., & Lin, N. (2020). YawMMF: Effective Mixed Model Functions. Available at: https://github.com/usplos/YawMMF.\n')
+
   eval(parse(text = paste0('mmff = model.matrix(~ ',FixEffects,', Data)')))
   IVName = gsub(pattern = ':',replacement = '_',x = colnames(mmff)[2:ncol(mmff)]) %>%
     substr(x = ., start = 1, stop = nchar(.))
