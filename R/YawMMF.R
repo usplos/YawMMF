059c55666dc3515d7a23a9bb869fcc78e7cd78cd
Yaw <zgy070620@163.com>
2021-01-08 08:19:11 +0800

    Create YawMMF.R

---
+++ R/YawMMF.R
@@ -0,0 +1,4 @@
+.onAttach=function(libname, pkgname) {
+  cat('######################\n\nPlease cite:\n')
+  cat('Zhang, G., Li, X., & Lin, N. (2020). YawMMF: Effective Mixed Model Functions. Available at: https://github.com/usplos/YawMMF.\n\n######################\n')
+}
