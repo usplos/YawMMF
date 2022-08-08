b1bd7c331f03bef3ba1b9d094e618744df04218b
Yaw <zgy070620@163.com>
2022-08-08 14:48:09 +0800

    Add files via upload

---
+++ R/ggCorrelationMatrix.R
@@ -0,0 +1,115 @@
+ggCorrelationMatrix = function(data, Arrange = 'full',
+                               color_low = 'dodgerblue4', color_mid = 'white',color_high = 'red',midpoint = 0,
+                               gridgap = 0,
+                               x_lab = NULL, y_lab = NULL, x_title = '', y_title = '', legend_title = 'Correlations',
+                               value_labeled = F){
+  library(ggplot2)
+  library(dplyr)
+  library(tibble)
+  
+  if(is.null(x_lab)){
+    if(Arrange == 'full') x_lab = colnames(data)
+    if(Arrange == 'leftlow') x_lab = colnames(data)[-length(colnames(data))]
+    if(Arrange == 'leftup') x_lab = colnames(data)[-1]
+  } 
+  
+  if(is.null(y_lab)){
+    if(Arrange == 'full') y_lab = rownames(data)
+    if(Arrange == 'leftlow') y_lab = rownames(data)[-1]
+    if(Arrange == 'leftup') y_lab = rownames(data)[-length(colnames(data))]
+  } 
+    
+  dfp = tibble()
+  Rname = rownames(data)
+  Cname = colnames(data)
+  
+  if(Arrange == 'full'){
+    for(x in 1:length(Cname)){
+      for(y in 1:length(Rname)){
+        if(x != y){
+          x_p = x
+          y_p = length(Rname)-y+1
+          dfp = bind_rows(dfp,
+                          tibble(x_p, y_p, value = data[x,y]))
+        }
+      }
+    }
+  }
+  
+  if(Arrange == 'leftlow'){
+    for(x in 1:(length(Cname)-1)){
+      for(y in (x+1):length(Rname)){
+        if(x != y){
+          x_p = x
+          y_p = length(Rname)-y+1
+          dfp = bind_rows(dfp,
+                          tibble(x_p, y_p, value = data[x,y]))
+        }
+      }
+    }
+  }
+  
+  if(Arrange == 'leftup'){
+    for(x in 2:length(Cname)){
+      for(y in 1:(x-1)){
+        if(x != y){
+          x_p = x
+          y_p = length(Rname)-y
+          dfp = bind_rows(dfp,
+                          tibble(x_p, y_p, value = data[x,y]))
+        }
+      }
+    }
+  }
+  
+  p = ggplot(data = dfp, aes(x = x_p, y = y_p))+
+    geom_rect(aes(fill = value,xmin = x_p - (.5 - gridgap/2), xmax = x_p + (.5 - gridgap/2), ymin = y_p-(.5 - gridgap/2), ymax = y_p+(.5 - gridgap/2)))+
+    scale_fill_gradient2(low = 'blue',mid = 'white',high = 'red',midpoint = 0)+
+    theme_minimal()+
+    theme(panel.grid = element_blank())+
+    scale_x_continuous(breaks = unique(dfp$x_p) %>% sort(),labels = x_lab)+
+    scale_y_continuous(breaks = unique(dfp$y_p) %>% sort(), labels = y_lab[length(y_lab):1])+
+    labs(x = x_title,
+         y = y_title,
+         fill = legend_title)+
+    theme(axis.title = element_text(size=14, face = 'bold'),
+          axis.text.y = element_text(size=12, face = 'bold'),
+          axis.text.x = element_text(size=12, face = 'bold',angle = 45,vjust = 0.9))
+  if(isTRUE(value_labeled)){
+    p = p + geom_text(data = dfp %>% mutate(value2 = round(value,2) %>% as.character(),
+                                            value_label = ifelse(value > 0, 
+                                                                 substr(value2,2,4),
+                                                                 paste0('-',substr(value2,3,5)))),
+                      aes(x = x_p, y = y_p, label = value_label))
+  }
+  return(p)
+}
+
+
+
+# ggCorrelationMatrix(data = cor(USJudgeRatings),Arrange = 'full',
+#                     color_low = 'dodgerblue',
+#                     color_mid = 'white',color_high = 'red',midpoint = 0,
+#                     x_lab = NULL,y_lab = NULL,
+#                     x_title = '',y_title = '',legend_title = 'Correlation',
+#                     gridgap = 0.05, value_labeled = T)
+# 
+
+
+
+
+
+
+
+# data = cor(USJudgeRatings)
+# Arrange = 'leftup' # c('full','leftlow','leftup')
+# color_low = 'dodgerblue4'
+# color_mid = 'white'
+# color_high = 'red'
+# midpoint = 0
+# gridgap = .1
+# x_lab = NULL
+# y_lab = NULL
+# x_title = ''
+# y_title = ''
+# legend_title = 'Correlations'
---
+++ R/ggViolinPointMeanSE.R
@@ -0,0 +1,84 @@
+ggViolinPointMeanSE = function(data, cond, Y, Group = NULL, Levels = NULL, Labels = NULL, 
+                               colors = c('#aa001a','#d35219','dodgerblue4','dodgerblue3')){
+  library(ggplot2)
+  library(dplyr)
+  
+  names(data)[which(names(data) == cond)] = 'cond'
+  names(data)[which(names(data) == Y)] = 'Y'
+  if(is.null(Levels)){
+    data$cond = factor(data$cond)
+  }else{
+    if(is.null(Labels)){
+      data$cond = factor(data$cond, levels = Levels)
+    }else{
+      data$cond = factor(data$cond, levels = Levels, labels = Labels)
+    }
+  }
+  
+  if(is.null(Labels)) Labels = levels(data$cond)
+  
+  if(is.null(Group)){
+    dfp = data %>% 
+      rename(Ms = Y)
+  }else{
+    names(data)[which(names(data) == Group)] = 'Group'
+    dfp = data %>%
+      group_by(Group,cond) %>%
+      summarise(Ms = mean(Y))
+    
+  }
+  dfp2 = dfp %>% group_by(cond) %>% 
+    summarise(M = mean(Ms),
+              SE = sd(Ms)/sqrt(length(Ms)),
+              X = (as.numeric(cond)+1)/2)
+  
+  p = ggplot(data = dfp2,
+             aes(y = M,color = factor(cond), fill = factor(cond)))+
+    geom_errorbar(aes(ymax = M+SE, ymin = M-SE,x=X+.05),
+                  position = position_dodge(0.2), width = .05, show.legend = F)+
+    theme_classic()+
+    geom_point(aes(x=X+.05),size=1, shape=15,show.legend = F)+
+    geom_violin(data = dfp %>%
+                  mutate(X = (as.numeric(cond)+1)/2),
+                aes(x = X-.1, y = Ms,fill = factor(cond)),color=NA,size=1, alpha=.3,show.legend = F,
+                adjust=1.5, trim = F, width=.2)+
+    geom_point(data = dfp %>%
+                 mutate(X = (as.numeric(cond)+1)/2),
+               aes(x = X-.1, y = Ms,color = factor(cond)),size=1, alpha=.3,show.legend = F,
+               position = position_beeswarm())+
+    labs(y = Y,x='')+
+    scale_x_continuous(breaks = unique(dfp2$X),
+                       labels = Labels)+
+    theme(axis.text.x = element_text(face = 'bold',size=10),
+          axis.line.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.title.y = element_text(size=12,face = 'bold'),
+          axis.text.y = element_text(face = 'bold'))
+  if(length(colors) >= length(Labels)){
+    p = p+
+      scale_color_manual(values = colors[1:length(Labels)])+
+      scale_fill_manual(values = colors[1:length(Labels)])
+  }else{
+    cat("\n\n## ## ## ## ## ## ## ## ## ## \n",
+        "Since the number of colors that your set is less than that of levels of your condition, the function will use the default color palette of ggplot2.",
+        '\n## ## ## ## ## ## ## ## ## ## \n\n')
+  }
+  return(p)
+}
+
+ggViolinPointMeanSE(data = DemoData %>% mutate(Cond = paste(CondA, CondB,sep = '')),
+                    cond = 'Cond',
+                    Y = 'DV',
+                    Group = 'subj',
+                    Levels = c('A1B1','A2B1','A1B2','A2B2'),
+                    Labels = c('Condition1','Condition2','Condition3','Condition4'),
+                    colors = c('#aa001a','#d35219','dodgerblue4','dodgerblue3'))
+
+
+
+
+
+
+
+
+
